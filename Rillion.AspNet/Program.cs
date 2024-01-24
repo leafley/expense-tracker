ea04416cc23f6fe1f41a1a2669bd8584763ca0d5
Pierre Gunter <pierre.gunter@protonmail.com>
2024-01-24 16:55:03 +0100

    Add AspNet.Identity

--- Rillion.AspNet/Program.cs
+++ Rillion.AspNet/Program.cs
@@ -1,13 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser<long>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -23,5 +30,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapIdentityApi<IdentityUser<long>>();
 
 app.Run();
