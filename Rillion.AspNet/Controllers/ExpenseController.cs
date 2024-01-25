6484854d84e164a561052eaff6a869941cd81c1f
Pierre Gunter <pierre.gunter@protonmail.com>
2024-01-25 16:11:27 +0100

    Add the expense endpoints

---
+++ Rillion.AspNet/Controllers/ExpenseController.cs
@@ -0,0 +1,41 @@
+
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rillion.Application.Expense.Commands;
+using Rillion.Application.Expense.Queries;
+
+namespace Rillion.AspNet.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class ExpenseController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ExpenseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPut]
+    public async Task<Domain.Entities.Expense> AddAsync([FromBody] CreateExpense command, CancellationToken cancellationToken) =>
+        await _mediator.Send(command, cancellationToken);
+
+    [HttpPost("amount")]
+    public async Task<Domain.Entities.Expense> UpdateAmountAsync([FromBody] UpdateExpenseAmount command, CancellationToken cancellationToken) =>
+        await _mediator.Send(command, cancellationToken);
+
+    [HttpPost("category")]
+    public async Task<Domain.Entities.Expense> UpdateCategoryAsync([FromBody] UpdateExpenseCategory command, CancellationToken cancellationToken) =>
+        await _mediator.Send(command, cancellationToken);
+
+    [HttpDelete]
+    public async Task<Domain.Entities.Expense> DeleteAsync([FromBody] DeleteExpense command, CancellationToken cancellationToken) =>
+        await _mediator.Send(command, cancellationToken);
+
+    [HttpGet]
+    public async Task<IEnumerable<Domain.Entities.Expense>> GetAsync(QueryExpensePage query, CancellationToken cancellationToken) =>
+        await _mediator.Send(query, cancellationToken);
+}
