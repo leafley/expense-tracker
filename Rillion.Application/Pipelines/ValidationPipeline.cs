c40f49510b58abbd285fac4e62d464fc335e4be6
Pierre Gunter <pierre.gunter@protonmail.com>
2024-01-25 11:08:04 +0100

    Add the validation pipeline

---
+++ Rillion.Application/Pipelines/ValidationPipeline.cs
@@ -0,0 +1,28 @@
+using FluentValidation;
+using MediatR;
+
+public class ValidationPipeline<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPipeline(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var failures = _validators
+            .Select(n => n.Validate(request))
+            .Where(n => !n.IsValid)
+            .SelectMany(n => n.Errors)
+            .ToList();
+
+        if (failures.Any())
+            throw new ValidationException(failures);
+
+        return next();
+    }
+}
