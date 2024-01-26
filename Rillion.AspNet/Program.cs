03de1795e3dc42a757016345d3971440e4a65f6d
Pierre Gunter <pierre.gunter@protonmail.com>
2024-01-26 10:13:20 +0100

    Remove Swashbuckle

--- Rillion.AspNet/Program.cs
+++ Rillion.AspNet/Program.cs
@@ -12,10 +12,6 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -25,12 +21,6 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser<long>>()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
